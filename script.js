15821c0b01e5ea248a13dc2446d3c039c71ed833
Hude1 <hude1300@gmail.com>
2023-11-21 20:07:22 +0000

    PWD generate Logic Completed

--- script.js
+++ script.js
@@ -5,9 +5,45 @@ function generatePassword(){
   let pwdLen = prompt("How many characters would you like to your password to be? (8-128 characters)")
   if(pwdLen < 8 || pwdLen > 128){
     alert("Password length must be between 8 and 128 characters")
+    return ""
   }
 
-  return ""
+  
+  let includelower = confirm("Would you like to include lowercase characters?")
+  let includeupper = confirm("Would you like to include uppercase characters?")
+  
+  let includenumeric = confirm("Would you like to include numeric characters?")
+  let includespecial = confirm("Would you like to include special characters?")
+  
+
+  if(!includelower && !includeupper && !includenumeric && !includespecial ){
+    alert("You must select atleast one character type.")
+    return ""
+  }
+
+
+  let lowerCaseChars = "abcdefghijklmnopqrstuvwxyz"
+  let upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  let numericChars = "0123456789"
+  let specialChars = "!@#$%^&*()_+"
+
+  let allChars = ""
+  if(includelower) allChars += lowerCaseChars
+  if(includeupper) allChars += upperCaseChars
+  if(includenumeric) allChars += numericChars
+  if(includespecial) allChars += specialChars
+  
+
+  let passwrod = ""
+  for(let i=0; i<pwdLen; i++){
+    let randomIndex = Math.floor(Math.random() * allChars.length)
+    passwrod += allChars[randomIndex]
+  }
+
+  return generatePassword
+
+
+
 }
 
 
