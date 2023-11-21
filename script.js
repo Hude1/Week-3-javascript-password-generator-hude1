9d855789310c438c2c53968073137e623dbebaae
Hude1 <hude1300@gmail.com>
2023-11-21 19:58:33 +0000

    Password Length

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Assignment code here
 
+// Function to generate password
+function generatePassword(){
+  let pwdLen = prompt("How many characters would you like to your password to be? (8-128 characters)")
+  if(pwdLen < 8 || pwdLen > 128){
+    alert("Password length must be between 8 and 128 characters")
+  }
+
+  return ""
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
